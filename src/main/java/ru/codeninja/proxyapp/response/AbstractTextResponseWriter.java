347ffb5ee47d174f48287a31fe1bd7d000a56818
codeninja-ru <aesuaS8vooKahog0>
2015-02-10 00:01:29 +0300

    fixed css modification

--- src/main/java/ru/codeninja/proxyapp/response/AbstractTextResponseWriter.java
+++ src/main/java/ru/codeninja/proxyapp/response/AbstractTextResponseWriter.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -27,14 +26,14 @@ public abstract class AbstractTextResponseWriter implements ResponseWriter {
             }
         }
 
-        if (charset != null) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), charset));
-            String currentUrl = connection.getURL().toString();
-            output.setCharacterEncoding(charset);
-            contentModifier.modify(currentUrl, reader, output.getWriter());
-            reader.close();
-        } else {
-            l.log(Level.WARNING, "cannot determinate charset for content type '{}'", contentType);
+        if (charset == null) {
+            charset = "utf-8"; //todo hm...
         }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), charset));
+        String currentUrl = connection.getURL().toString();
+        output.setCharacterEncoding(charset);
+        contentModifier.modify(currentUrl, reader, output.getWriter());
+        reader.close();
     }
 }

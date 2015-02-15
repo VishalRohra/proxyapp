f35324d7afb8596964b224237cc4290d4dffad83
codeninja-ru <aesuaS8vooKahog0>
2015-02-16 00:55:06 +0300

    started cookies

--- src/main/java/ru/codeninja/proxyapp/cookies/SecuredCookiesRule.java
+++ src/main/java/ru/codeninja/proxyapp/cookies/SecuredCookiesRule.java
@@ -8,13 +8,18 @@ import java.net.HttpURLConnection;
  * Created by vital on 14.02.15.
  */
 public class SecuredCookiesRule implements CookiesRule {
+
+    static final String COOKIE_HTTP_HEADER = "Cookie";
+
     @Override
     public void sendCookies(HttpServletRequest request, HttpURLConnection connection) {
-
+        String cookies = request.getHeader(COOKIE_HTTP_HEADER);
+        connection.setRequestProperty(COOKIE_HTTP_HEADER, cookies);
     }
 
     @Override
     public void receiveCookies(HttpServletResponse response, HttpURLConnection cookiesSource) {
-
+        String cookies = cookiesSource.getRequestProperty(COOKIE_HTTP_HEADER);
+        response.setHeader(COOKIE_HTTP_HEADER, cookies);
     }
 }

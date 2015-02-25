64c05b604746e610a2df93af999ba21862ce22f5
codeninja-ru <aesuaS8vooKahog0>
2015-02-25 21:46:27 +0300

    refactored

--- src/main/java/ru/codeninja/proxyapp/connection/UrlConnectionFactory.java
+++ src/main/java/ru/codeninja/proxyapp/connection/UrlConnectionFactory.java
@@ -1,11 +1,14 @@
 package ru.codeninja.proxyapp.connection;
 
+import ru.codeninja.proxyapp.header.RequestHeadersManager;
+
 /**
  * Created by vital on 11.02.15.
  */
 public class UrlConnectionFactory {
-    static final GetRequestUrlConnection GET_REQUEST_URL_CONNECTION = new GetRequestUrlConnection();
-    static final PostRequestUrlConnection POST_REQUEST_URL_CONNECTION = new PostRequestUrlConnection();
+    static final RequestHeadersManager REQUEST_HEADERS_MANAGER = new RequestHeadersManager();
+    static final GetRequestUrlConnection GET_REQUEST_URL_CONNECTION = new GetRequestUrlConnection(REQUEST_HEADERS_MANAGER);
+    static final PostRequestUrlConnection POST_REQUEST_URL_CONNECTION = new PostRequestUrlConnection(REQUEST_HEADERS_MANAGER);
 
     public UrlConnection get(HttpMethod method) {
         switch (method) {
